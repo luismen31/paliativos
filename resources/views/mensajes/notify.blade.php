48806ca33f57fbfc3f8892a405151b99c4d40756
luismen31 <luis2531@gmail.com>
2015-09-21 08:41:03 -0500

    Modificacion de notificacion

--- resources/views/mensajes/notify.blade.php
+++ resources/views/mensajes/notify.blade.php
@@ -3,10 +3,15 @@
 	<script type="text/javascript">
 		$(function(){
 		    createNoty("{{ $mensaje }}", "{{ $tipo }}");
+		    //cerrar notificacion cliqueando el boton "x"
 		    $('.page-alert .close').click(function(e) {
 		        e.preventDefault();
 		        $(this).closest('.page-alert').slideUp();
 		    });
+		    //cerrar notificacion despues de 5 seg. 
+		    setTimeout(function() {
+		        $(".page-alert .close").closest('.page-alert').slideUp();
+		    },5000);
 		});
 	</script>			
 @append
